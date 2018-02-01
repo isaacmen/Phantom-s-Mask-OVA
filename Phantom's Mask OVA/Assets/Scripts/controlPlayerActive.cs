a5803b2b3ab2af1d2194f75d4bc20cd5706d4b05
Abigail Jimenez <abigaipj@uci.edu>
2018-02-01 12:56:49 -0800

    Added scripts

---
+++ Phantom's Mask OVA/Assets/Scripts/controlPlayerActive.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlPlayerActive : MonoBehaviour {
+    public GameObject p1;
+    public GameObject p2;
+    public GameObject p3;
+
+    private int counter; //keeps track of counter, which character should move
+
+	// Use this for initialization
+	void Start () {
+        //Enable Character 1 first, disable chars 2 and 3
+        //Getting the script that controls the player and disabling it for 2 and 3
+        p1.GetComponent<playerControler>().enabled = true;
+        p2.GetComponent<playerControler>().enabled = false;
+        p3.GetComponent<playerControler>().enabled = false;
+        counter = 2;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            if (counter == 1) //Character 1 is in control
+            {
+                p1.GetComponent<playerControler>().enabled = true;
+                p2.GetComponent<playerControler>().enabled = false;
+                p3.GetComponent<playerControler>().enabled = false;
+                counter++;
+            }
+            else if (counter == 2) //Character 2 is in control
+            {
+                p1.GetComponent<playerControler>().enabled = false;
+                p2.GetComponent<playerControler>().enabled = true;
+                p3.GetComponent<playerControler>().enabled = false;
+                counter++;
+            }
+            else if (counter == 3) //Character 3 is in control
+            {
+                p1.GetComponent<playerControler>().enabled = false;
+                p2.GetComponent<playerControler>().enabled = false;
+                p3.GetComponent<playerControler>().enabled = true;
+                counter = 1; //reset
+            }
+        }
+    }
+}
---
+++ Phantom's Mask OVA/Assets/Scripts/playerControler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControler : MonoBehaviour {
+
+    private Rigidbody2D myRigidbody;
+
+    [SerializeField]
+    private float speed;
+
+	// Use this for initialization
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        handleMovement(horizontal);
+    }
+
+    private void handleMovement(float horizontal)
+    {
+        myRigidbody.velocity = new Vector2(horizontal * speed,myRigidbody.velocity.y); //x-val -1, y-val 0
+    }
+}
