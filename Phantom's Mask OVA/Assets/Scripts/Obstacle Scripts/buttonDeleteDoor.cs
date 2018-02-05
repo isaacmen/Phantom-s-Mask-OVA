0b42f83fcaa3cdeb2be65bc54dd6a3fa7f45a37c
Abigail Jimenez <abigaipj@uci.edu>
2018-02-05 14:21:14 -0800

    Door Button Machine, Robbie, Veronica, Jonathan
    -Created a Prefab for the Machines named openDoorController. It only works if Robbie is the one pressing the button
    -Created Robbie, Veronica,  and Jonathan gameObjects with basic movement, including switching between eachother

---
+++ Phantom's Mask OVA/Assets/Scripts/Obstacle Scripts/buttonDeleteDoor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonDeleteDoor : MonoBehaviour {
+    public GameObject door; //Get the gameobject door this will control
+    private static bool doorDestroyed = false; //Keeps track if the door was already destroyed
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay2D(Collider2D player)
+    {
+        //Debug.Log(player.gameObject.name + "\n");
+        //Check the door hasn't been destroyed and if Robbie is touching it
+        if(doorDestroyed == false && player.gameObject.name == "Robbie") 
+        {
+            //If it's Robbie and he's pressing Spacebar(special move)
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                //Destroy the Door gameobject
+                Destroy(door);
+                //Debug.Log("Door was Destroyed\n");
+                doorDestroyed = true;
+            }
+        }
+    }
+}
