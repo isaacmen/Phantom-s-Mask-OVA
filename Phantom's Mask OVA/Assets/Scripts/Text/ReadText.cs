08a3b51e651e32639559911ff4cae588771f4ea7
Isaac <isacmen@gmail.com>
2018-02-05 19:50:27 -0800

    Added Text Reader

---
+++ Phantom's Mask OVA/Assets/Scripts/Text/ReadText.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+
+public class ReadText : MonoBehaviour {
+
+	public string filename;
+	public UnityEngine.UI.Text Displaytext;
+
+	private string[] lines;
+	private int max;
+	private int currentline = 0;
+
+	string[] GetLines (string filename) {
+		string path = "Assets/Text/" + filename;
+		string[] result = File.ReadAllLines (path);
+
+		return result;
+	}
+		
+
+	// Use this for initialization
+	void Start () {
+		lines = GetLines (filename);
+		max = lines.Length;
+		Displaytext.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentline < max && Input.GetKeyDown ("space")) {
+			Displaytext.text = lines [currentline];
+			currentline++;
+		} else if (currentline >= max && Input.GetKeyDown ("space")) {
+			Displaytext.text = "";
+		}
+	}
+}
