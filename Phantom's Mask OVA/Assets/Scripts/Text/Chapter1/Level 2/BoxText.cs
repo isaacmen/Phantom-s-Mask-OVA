1b9553cc5d6463028f8cd381fadad87596532913
Wesley Tsai <wesley7502@gmail.com>
2018-03-03 22:20:03 -0800

    Attempt on getting text to level 2
    My honest attempt to get text working on level 2, The scene text works fine, but I am unable to get robbie and caroline to interact

---
+++ Phantom's Mask OVA/Assets/Scripts/Text/Chapter1/Level 2/BoxText.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Controls the Text script on the Breakable Box. Handles plyer interaction with the box
+ */ 
+public class BoxText : MonoBehaviour {
+	
+	//Strings that have the file names
+    private const string BoxCaroline = "Box-Caroline.txt";
+    private const string BoxRobbie = "Box-Robbie.txt";
+	
+	//Checks if Robbie and Caroline have touched the box already and interacted with it
+    private static bool robbieTouch = false;
+    private static bool carolineTouch = false;
+	
+	//Checks if they are currently touching the box
+    private bool carolineTouching = false;
+    private bool robbieTouching = false;
+    private bool yvetteTouching = false;
+	
+	// Update is called once per frame
+	void Update () {
+		if (robbieTouching && !robbieTouch && GameObject.Find("controlPlayerActive").GetComponent<controlPlayerActive>().isActiveName() == "Robbie" && Input.GetKey("e"))
+        {
+            gameObject.GetComponent<ReadText>().active = true;
+            gameObject.GetComponent<ReadText>().filename = BoxRobbie;
+            robbieTouch = true;
+        }
+		
+		else if (carolineTouching && !carolineTouch && GameObject.Find("controlPlayerActive").GetComponent<controlPlayerActive>().isActiveName() == "Caroline" && Input.GetKey("e"))
+        {
+            gameObject.GetComponent<ReadText>().active = true;
+            gameObject.GetComponent<ReadText>().filename = BoxCaroline;
+            carolineTouch = true;
+        }
+
+        else if (yvetteTouching && Input.GetKey("e") && GameObject.Find("controlPlayerActive").GetComponent<controlPlayerActive>().isActiveName() == "Yvette")
+        {
+            gameObject.GetComponent<BoxText>().enabled = false;
+        }
+	}
+	
+	void OnCollisionEnter2D(Collision2D player)
+    {
+        //Check if Caroline is touching the box
+        if (player.gameObject.name == "Caroline")
+            carolineTouching = true;
+
+        //Check if Robbie is touching the box
+        else if (player.gameObject.name == "Robbie"){
+			gameObject.GetComponent<ReadText>().filename = BoxRobbie;
+            robbieTouching = true;
+			
+		}
+
+        else if (player.gameObject.name == "Yvette")
+            yvetteTouching = true;
+    }
+	
+	private void OnCollisionExit2D(Collision2D player)
+    {
+        //Check if Caroline is touching the box
+        if (player.gameObject.name == "Caroline")
+            carolineTouching = false;
+
+        //Check if Robbie is touching the box
+        else if (player.gameObject.name == "Robbie")
+            robbieTouching = false;
+
+        //Check if Yvette is touching the box
+        else if (player.gameObject.name == "Yvette")
+            yvetteTouching = false;
+    }
+}
