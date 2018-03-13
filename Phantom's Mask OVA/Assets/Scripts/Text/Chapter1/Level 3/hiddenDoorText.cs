ee7d6d55fa9f0f448947606049beb1788a2e49d5
Wesley Tsai <wesley7502@gmail.com>
2018-03-13 01:11:16 -0700

    Text for Chapter 1 level 3
    Enabled texts for level 3 of chapter 1

---
+++ Phantom's Mask OVA/Assets/Scripts/Text/Chapter1/Level 3/hiddenDoorText.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hiddenDoorText : MonoBehaviour {
+
+	 //Strings that have the file names
+    private const string TreeCaroline = "CarolineDetect-HiddenDoor.txt";
+
+    //Checks if Caroline have touched the box already and interacted with it
+    private static bool carolineTouch = false;
+
+    //Checks if they are currently touching the box
+    private bool carolineTouching = false;
+    private bool robbieTouching = false;
+    private bool yvetteTouching = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (robbieTouching && Input.GetKey("e") && GameObject.Find("controlPlayerActive").GetComponent<controlPlayerActive>().isActiveName() == "Robbie")
+        {
+            gameObject.GetComponent<treeText>().enabled = false;
+        }
+
+        else if (carolineTouching && !carolineTouch && GameObject.Find("controlPlayerActive").GetComponent<controlPlayerActive>().isActiveName() == "Caroline" && Input.GetKey("e"))
+        {
+            gameObject.GetComponent<ReadText>().active = true;
+            gameObject.GetComponent<ReadText>().filename = TreeCaroline;
+            carolineTouch = true;
+        }
+
+        else if (yvetteTouching && Input.GetKey("e") && GameObject.Find("controlPlayerActive").GetComponent<controlPlayerActive>().isActiveName() == "Yvette")
+        {
+            gameObject.GetComponent<treeText>().enabled = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D player)
+    {
+        //Check if Caroline is touching the car
+        if (player.gameObject.name == "Caroline")
+            carolineTouching = true;
+
+        //Check if Robbie is touching the car
+        else if (player.gameObject.name == "Robbie")
+            robbieTouching = true;
+
+        else if (player.gameObject.name == "Yvette")
+            yvetteTouching = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D player)
+    {
+        //Check if Caroline is touching the car
+        if (player.gameObject.name == "Caroline")
+            carolineTouching = false;
+
+        //Check if Robbie is touching the car
+        else if (player.gameObject.name == "Robbie")
+            robbieTouching = false;
+
+        //Check if Yvette is touching the car
+        else if (player.gameObject.name == "Yvette")
+            yvetteTouching = false;
+    }
+}
