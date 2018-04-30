cb61cc706dc7151d8cfa0272c49b70b27b54bed9
Wesley Tsai <wesley7502@gmail.com>
2018-04-30 12:00:53 -0700

    PARTIAL - Text for Chapter 3 Level 1 and Level 2
    Added the text interaction for Level 1 and Level 2, most noticably the pushable box

---
+++ Phantom's Mask OVA/Assets/Scripts/Text/Chapter3/PushableBoxText.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushableBoxText : MonoBehaviour {
+
+	//Strings that have the file names
+    private const string TextCaroline = "MovingBoxes_Caroline.txt";
+    private const string TextYvette = "MovingBoxes_Yvette.txt";
+
+    //Checks if they are currently touching the box
+    private bool carolineTouching = false;
+    private bool yvetteTouching = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (carolineTouching && GameObject.Find("controlPlayerActive").GetComponent<controlPlayerActive>().isActiveName() == "Caroline" && Input.GetKey("e"))
+        {
+            gameObject.GetComponent<ReadText>().active = true;
+            gameObject.GetComponent<ReadText>().filename = TextCaroline;
+        }
+
+        else if (yvetteTouching && Input.GetKey("e") && GameObject.Find("controlPlayerActive").GetComponent<controlPlayerActive>().isActiveName() == "Yvette")
+        {
+            gameObject.GetComponent<ReadText>().active = true;
+            gameObject.GetComponent<ReadText>().filename = TextYvette;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D player)
+    {
+        //Check if Caroline is touching the car
+        if (player.gameObject.name == "Caroline")
+            carolineTouching = true;
+
+        else if (player.gameObject.name == "Yvette")
+            yvetteTouching = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D player)
+    {
+        //Check if Caroline is touching the car
+        if (player.gameObject.name == "Caroline")
+            carolineTouching = false;
+
+        //Check if Yvette is touching the car
+        else if (player.gameObject.name == "Yvette")
+            yvetteTouching = false;
+    }
+}
