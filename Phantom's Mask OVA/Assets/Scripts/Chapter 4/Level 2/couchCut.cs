0456c70c24f8231ae356985d7fa7f0165d2ebd73
Wesley Tsai <wesley7502@gmail.com>
2018-05-04 23:35:33 -0700

    Chapter 4 Level 2 Part 1
    -Added images to accomodate the level
    -Chair mechanism implemented
    -Clock mechanism partially implemented
    -Replaced the Johnathan sprite with veronica

---
+++ Phantom's Mask OVA/Assets/Scripts/Chapter 4/Level 2/couchCut.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class couchCut : MonoBehaviour {
+	
+	private bool isInvisible; //Whether the breakPoint is invisible or not.
+	private bool veronicaTouching;
+	public bool broken;
+	
+	// Use this for initialization
+	void Start () {
+		isInvisible = true;
+		broken = false;
+		gameObject.GetComponent<Renderer>().enabled = false;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isInvisible && Input.GetKeyDown("e") && veronicaTouching){ //If Caroline detects it while invisible, show up
+			if (GameObject.Find("controlPlayerActive").GetComponent<controlPlayerActive>().isActiveName() == "Veronica")
+            {
+			gameObject.GetComponent<Renderer>().enabled = true;
+			isInvisible = false;
+			}
+			
+		}
+	}
+	
+	
+	private void OnTriggerEnter2D(Collider2D player)
+    {
+        if (player.name == "Veronica")
+            veronicaTouching = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D player)
+    {
+        if (player.name == "Veronica")
+            veronicaTouching = false;
+
+    }
+}
