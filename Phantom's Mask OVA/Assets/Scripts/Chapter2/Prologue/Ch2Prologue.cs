9d89a8aa5c87a51563d62ffe3a0795c8c7e92d86
Isaac <isacmen@gmail.com>
2018-05-14 14:48:22 -0700

    god wtf

---
+++ Phantom's Mask OVA/Assets/Scripts/Chapter2/Prologue/Ch2Prologue.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ch2Prologue : MonoBehaviour {
+
+	public GameObject caroline;
+	public GameObject robbie;
+	public GameObject yvette;
+	public Text txt;
+
+	private GameObject reader;
+
+	private bool part0 = true;
+	private bool part1 = false;
+	private bool part2 = false;
+	private bool part3 = false;
+	private bool part4 = false;
+
+	private int timer = 0;
+
+	// Use this for initialization
+	void Start () {
+		reader = GameObject.FindGameObjectWithTag("textbox");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		if (part0) {
+			reader.GetComponent<ReadText> ().active = true;
+			reader.GetComponent<ReadText> ().filename = "Chapter2-Start(1).txt";
+			if (txt.text == "") {
+				for (int i = 0; i < 40; i++) {
+					caroline.transform.Translate (10 * Time.deltaTime, 0, 0);
+					robbie.transform.Translate (10 * Time.deltaTime, 0, 0);
+					yvette.transform.Translate (10 * Time.deltaTime, 0, 0);
+					if (i == 39) {
+						part0 = false;
+						part1 = true;
+					}
+				}
+			}
+		}
+
+		if (part1 && !part0) {
+			reader.GetComponent<ReadText> ().filename = "Chapter2-Employee_lounge(2).txt";
+			reader.GetComponent<ReadText> ().active = true;
+			timer++; 
+
+			if (txt.text == "" && timer > 5) {
+				for (int i = 0; i < 50; i++) {
+					robbie.transform.Translate (10 * Time.deltaTime, 0, 0);
+					if (i == 49) {
+						part1 = false;
+						part2 = true;
+						timer = 0;
+					}
+				}
+			}
+		}
+			
+		if (part2 && !part1) {
+			reader.GetComponent<ReadText>().active = true;
+			reader.GetComponent<ReadText>().filename = "Diary 1 (3).txt";
+			timer++;
+
+			if (txt.text == "" && timer > 5) {
+				for (int i = 0; i < 30; i++) {
+					yvette.transform.Translate (10 * Time.deltaTime, 0, 0);
+					caroline.transform.Translate (10 * Time.deltaTime, 0, 0);
+					if (i == 29) {
+						part2 = false;
+						part3 = true;
+						timer = 0;
+					}
+				}
+			}
+		}
+
+		if (part3) {
+			reader.GetComponent<ReadText>().active = true;
+			reader.GetComponent<ReadText>().filename = "Disscussion_After_Diary(4).txt";
+			timer++;
+			if (txt.text == "" && timer > 5) { 
+						part3 = false;
+						part4 = true;
+				}
+		}
+
+		if (part4) {
+			this.GetComponentInChildren<changeScene> ().active = true;
+		}
+	}
+}
+
