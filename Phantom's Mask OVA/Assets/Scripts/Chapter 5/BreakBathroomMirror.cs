1b8978ed980c9f9d0a7646398ad424ec434d0243
Abigail Jimenez <abigaipj@uci.edu>
2018-05-24 18:38:41 -0700

    Changes before pulling

--- Phantom's Mask OVA/Assets/Scripts/Chapter 5/BreakBathroomMirror.cs
+++ Phantom's Mask OVA/Assets/Scripts/Chapter 5/BreakBathroomMirror.cs
@@ -3,14 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BreakBathroomMirror : MonoBehaviour {
+    private bool isBreakable;
+    private bool yvetteTouching;
+    public GameObject lever;
+
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        isBreakable = false;
+        yvetteTouching = false;
+        lever.GetComponent<Renderer>().enabled= false;
+        lever.GetComponent<BoxCollider2D>().enabled = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (isBreakable)
+            checkYvetteTouching();
+    }
+
+    void checkYvetteTouching()
+    {
+        if (yvetteTouching && Input.GetKey("e"))
+        {
+            if (GameObject.Find("controlPlayerActive").GetComponent<controlPlayerActive5people>().isActiveName() == "Yvette")
+            {
+                // Activate the renderer for the lever
+                lever.GetComponent<Renderer>().enabled = true;
+                lever.GetComponent<BoxCollider2D>().enabled = true;
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void paintingSeen()
+    {
+        isBreakable = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Yvette")
+            yvetteTouching = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.name == "Yvette")
+            yvetteTouching = false;
+    }
 }
---
+++ Phantom's Mask OVA/Assets/Scripts/Chapter 5/MirrorLever.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorLever : MonoBehaviour {
+    private bool leverIsTriggered;
+    private bool robbieTouching;
+
+	// Use this for initialization
+	void Start () {
+        leverIsTriggered = false;
+        robbieTouching = false;
+        
+    }
+	
+    private void checkTriggerLever()
+    {
+        if(robbieTouching && Input.GetKey("e"))
+        {
+            if (GameObject.Find("controlPlayerActive").GetComponent<controlPlayerActive5people>().isActiveName() == "Robbie")
+            {
+                //Turn on the altar
+                Debug.Log("GOT IT");
+                leverIsTriggered = false;
+            }
+        }
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (leverIsTriggered == false)
+            checkTriggerLever();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name == "Robbie")
+        {
+            robbieTouching = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.name == "Robbie")
+        {
+            robbieTouching = false;
+        }
+    }
+
+
+}
