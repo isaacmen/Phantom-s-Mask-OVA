a2ecfa43da35994c25e9c3f76c61b9ce2860e285
Abigail Jimenez <abigaipj@uci.edu>
2018-05-28 16:34:13 -0700

    Title screen changes

---
+++ Phantom's Mask OVA/Assets/Scripts/Main Menu/FlickeringLight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour {
+    Light flicker;
+    public float min;
+    public float max;
+    // Use this for initialization
+    void Start()
+    {
+        flicker = GetComponent<Light>();
+        StartCoroutine(Flashing());
+    }
+
+    IEnumerator Flashing()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(min, max));
+            flicker.enabled = !flicker.enabled;
+        }
+    }
+}
---
+++ Phantom's Mask OVA/Assets/Scripts/Main Menu/WaveringLight.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveringLight : MonoBehaviour {
+    Light flicker;
+    public float rate;
+    public float max;
+    private bool increasing;
+    private bool decreasing;
+    private float intensity;
+    // Use this for initialization
+    void Start () {
+        flicker = GetComponent<Light>();
+        flicker.intensity = 0.1f;
+        intensity = 0;
+        increasing = true;
+        decreasing = false;
+        StartCoroutine(Flashing());
+    }
+
+
+    IEnumerator Flashing()
+    { 
+        while (true)
+        {
+            yield return new WaitForSeconds(rate);
+            Debug.Log(flicker.intensity);
+            if (intensity >= max)
+            {
+                decreasing = true;
+                increasing = false;
+                flicker.intensity -=  0.05f;
+                intensity -= 0.05f;
+            }
+            else if(intensity <= 0)
+            {
+                increasing = true;
+                decreasing = false;
+                flicker.intensity += 0.05f;
+                intensity += 0.05f;
+            }
+            else if (increasing)
+            {
+                flicker.intensity += 0.05f;
+                intensity += 0.05f;
+            }
+
+            else if (decreasing)
+            {
+                flicker.intensity -= 0.05f;
+                intensity -= 0.05f;
+            }
+            
+        }
+    }
+}
