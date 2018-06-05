275d066087d8e49e1ff3e11b1edef3c305f0abaf
Abigail Jimenez <abigaipj@uci.edu>
2018-06-04 18:48:34 -0700

    Fixed title Screen
    -Title Screen is finished

---
+++ Phantom's Mask OVA/Assets/Scripts/CarolineDetectSoundScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * This one is ONLY For ControlPlayerActive with 3 characters. There will be a different one for levels where there are
+ * More or less than 3 characters
+ * */
+public class CarolineDetectSoundScript : MonoBehaviour {
+    public GameObject ControlPlayerActive;
+
+
+	// Update is called once per frame
+	void Update () {
+        //Sound can play
+		if(ControlPlayerActive.GetComponent<controlPlayerActive>().isActiveName() == "Caroline")
+        {
+            if(GetComponent<AudioSource>().enabled == false)
+                 GetComponent<AudioSource>().enabled = true;
+        }
+
+        else
+        {
+            GetComponent<AudioSource>().enabled = false;
+        }
+	}
+}
