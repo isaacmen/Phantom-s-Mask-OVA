9728af30defcd1ff8fff198e279e657c30225c69
Isaac <isacmen@gmail.com>
2018-06-05 02:20:06 -0700

    Chapter2 Prologue Epilogue finished

--- Phantom's Mask OVA/Assets/Scripts/Text/Chapter2/Epilogue/Ch2Epilogue.cs
+++ Phantom's Mask OVA/Assets/Scripts/Text/Chapter2/Epilogue/Ch2Epilogue.cs
@@ -11,11 +11,19 @@ public class Ch2Epilogue : MonoBehaviour {
 	public GameObject chris;
 	public Text txt;
 
+	public GameObject ball;
+	public GameObject diary;
+
+	public AudioClip ballsound;
+	public AudioClip diarysound;
+
+	private AudioSource SFX;
 	private GameObject reader;
 
 	// Use this for initialization
 	void Start () {
 		reader = GameObject.FindGameObjectWithTag("textbox");
+		SFX = GameObject.Find ("SFX").GetComponent<AudioSource> ();
 
 		StartCoroutine(scene());
 	}
@@ -32,35 +40,52 @@ public class Ch2Epilogue : MonoBehaviour {
 		yield return new WaitWhile(() => txt.text != "");
 
 		while(yvette.transform.position.x < 0) {
+			yvette.GetComponent<Animator>().SetInteger("Direction", 1);
 			yvette.transform.Translate (10 * Time.deltaTime, 0, 0);
 			yield return new WaitForEndOfFrame();
 		}
+		yvette.GetComponent<Animator>().SetInteger("Direction", 0);
 
-		while(robbie.transform.position.x < 1) {
+		while(caroline.transform.position.x < -1) {
+			robbie.GetComponent<Animator>().SetInteger("Direction", 1);
+			caroline.GetComponent<Animator>().SetInteger("Direction", 1);
 			robbie.transform.Translate (10 * Time.deltaTime, 0, 0);
 			caroline.transform.Translate (10 * Time.deltaTime, 0, 0);
 			yield return new WaitForEndOfFrame();
 		}
+		robbie.GetComponent<Animator>().SetInteger("Direction", 0);
+		caroline.GetComponent<Animator>().SetInteger("Direction", 0);
 
 		reader.GetComponent<ReadText> ().filename = "Chapter2-End(2).txt";
 		reader.GetComponent<ReadText> ().active = true;
 		yield return new WaitForSeconds(1);
 		yield return new WaitWhile(() => txt.text != "");
 
-		while(yvette.transform.position.x < 6) {
-			yvette.transform.Translate (10 * Time.deltaTime, 0, 0);
+		while(yvette.transform.position.x < 3.5) {
+			yvette.GetComponent<Animator>().SetInteger("Direction", 1);
+			yvette.transform.Translate (5 * Time.deltaTime, 0, 0);
 			yield return new WaitForEndOfFrame();
 		}
+		yvette.GetComponent<Animator>().SetInteger("Direction", 0);
+
+		ball.SetActive (false);
+		SFX.clip = ballsound;
+		SFX.Play ();
+		yield return new WaitForSeconds(2);
+
+		chris.transform.Rotate (0.0f, 0.0f, 90.0f);
+		chris.transform.position = new Vector3 (2.5f, -2.5f, 0.0f);
+		yvette.transform.localScale = new Vector3 (-0.75f, 0.75f, 0.75f);
+		chris.GetComponent<Animator> ().enabled = true;
 
 		reader.GetComponent<ReadText> ().filename = "Chapter2-End(3).txt";
 		reader.GetComponent<ReadText> ().active = true;
 		yield return new WaitForSeconds(1);
 		yield return new WaitWhile(() => txt.text != "");
 
-		while(chris.transform.position.x < 10) {
-			chris.transform.Translate (10 * Time.deltaTime, 0, 0);
-			yield return new WaitForEndOfFrame();
-		}
+		diary.SetActive(true);
+		SFX.clip = diarysound;
+		SFX.Play ();
 
 		reader.GetComponent<ReadText> ().filename = "Diary2(4).txt";
 		reader.GetComponent<ReadText> ().active = true;
@@ -76,6 +101,23 @@ public class Ch2Epilogue : MonoBehaviour {
 		reader.GetComponent<ReadText> ().active = true;
 		yield return new WaitForSeconds(1);
 		yield return new WaitWhile(() => txt.text != "");
+
+		diary.SetActive(false);
+
+		yvette.transform.localScale = new Vector3 (0.75f, 0.75f, 0.75f);
+		chris.transform.localScale = new Vector3 (-0.75f, 0.75f, 0.75f);
+
+		while(caroline.transform.position.x < 13) {
+			robbie.GetComponent<Animator>().SetInteger("Direction", 2);
+			caroline.GetComponent<Animator>().SetInteger("Direction", 1);
+			chris.GetComponent<Animator>().SetInteger("Direction", 1);
+			yvette.GetComponent<Animator>().SetInteger("Direction", 1);
+			robbie.transform.Translate (-5 * Time.deltaTime, 0, 0);
+			caroline.transform.Translate (5 * Time.deltaTime, 0, 0);
+			yvette.transform.Translate (5 * Time.deltaTime, 0, 0);
+			chris.transform.Translate (5 * Time.deltaTime, 0, 0);
+			yield return new WaitForEndOfFrame();
+		}
 
 		this.GetComponentInChildren<changeScene> ().active = true;
 
--- Phantom's Mask OVA/Assets/Scripts/Text/Chapter2/Prologue/PrologueCh2.cs
+++ Phantom's Mask OVA/Assets/Scripts/Text/Chapter2/Prologue/PrologueCh2.cs
@@ -36,48 +36,65 @@ public class PrologueCh2 : MonoBehaviour {
 			reader.GetComponent<ReadText> ().active = true;
 			reader.GetComponent<ReadText> ().filename = "Chapter2-Start(1).txt";
 
-			for (int i = 0; i < 40; i++) {
-				caroline.transform.Translate (10 * Time.deltaTime, 0, 0);
-				robbie.transform.Translate (10 * Time.deltaTime, 0, 0);
+			while(robbie.transform.position.x < -4) {
+				caroline.GetComponent<Animator>().SetInteger("Direction", 1);
+				robbie.GetComponent<Animator>().SetInteger("Direction", 1);
+				caroline.transform.Translate (5 * Time.deltaTime, 0, 0);
+				robbie.transform.Translate (5 * Time.deltaTime, 0, 0);
 				yield return new WaitForEndOfFrame();
 			}
+			caroline.GetComponent<Animator>().SetInteger("Direction", 0);
+			robbie.GetComponent<Animator>().SetInteger("Direction", 0);
+
 			yield return new WaitWhile(() => txt.text != "");
 
 			reader.GetComponent<ReadText> ().filename = "Chapter2-Employee_lounge(2).txt";
 			reader.GetComponent<ReadText> ().active = true;
 
-			for (int i = 0; i < 45; i++) {
+			while(yvette.transform.position.x < -6) {
+				yvette.GetComponent<Animator>().SetInteger("Direction", 1);
 				yvette.transform.Translate (10 * Time.deltaTime, 0, 0);
 				yield return new WaitForEndOfFrame();
 			}
+			yvette.GetComponent<Animator>().SetInteger("Direction", 0);
 			yield return new WaitForSeconds(1);
 			yield return new WaitWhile(() => txt.text != "");
 
-			for (int i = 0; i < 50; i++) { 
-				robbie.transform.Translate (10 * Time.deltaTime, 0, 0);
+		while(robbie.transform.position.x < 0) { 
+				robbie.transform.Translate (5 * Time.deltaTime, 0, 0);
+				robbie.GetComponent<Animator>().SetInteger("Direction", 1);
 				yield return new WaitForEndOfFrame();
 			}
+			robbie.GetComponent<Animator>().SetInteger("Direction", 0);
 
 			reader.GetComponent<ReadText>().active = true;
 			reader.GetComponent<ReadText>().filename = "Diary 1 (3).txt";
 			yield return new WaitForSeconds(1);
 			yield return new WaitWhile(() => txt.text != "");
 
-			for (int i = 0; i < 30; i++) {
-				yvette.transform.Translate (10 * Time.deltaTime, 0, 0);
-				caroline.transform.Translate (10 * Time.deltaTime, 0, 0);
+		while(caroline.transform.position.x < -1) {
+				yvette.GetComponent<Animator>().SetInteger("Direction", 1);
+				caroline.GetComponent<Animator>().SetInteger("Direction", 1);
+				yvette.transform.Translate (5 * Time.deltaTime, 0, 0);
+				caroline.transform.Translate (5 * Time.deltaTime, 0, 0);
 				yield return new WaitForEndOfFrame();
 			}
+			caroline.GetComponent<Animator>().SetInteger("Direction", 0);
+			yvette.GetComponent<Animator>().SetInteger("Direction", 0);
 
 			reader.GetComponent<ReadText>().active = true;
 			reader.GetComponent<ReadText>().filename = "Disscussion_After_Diary(4).txt";
 			yield return new WaitForSeconds(1);
 			yield return new WaitWhile(() => txt.text != "");
 
-			for (int i = 0; i < 60; i++) {
-				caroline.transform.Translate (10 * Time.deltaTime, 0, 0);
-				robbie.transform.Translate (10 * Time.deltaTime, 0, 0);
-				yvette.transform.Translate (10 * Time.deltaTime, 0, 0);
+			while(robbie.transform.position.x > -10) {
+				yvette.GetComponent<Animator>().SetInteger("Direction", 2);
+				caroline.GetComponent<Animator>().SetInteger("Direction", 2);
+				robbie.GetComponent<Animator>().SetInteger("Direction", 2);
+
+				caroline.transform.Translate (-5 * Time.deltaTime, 0, 0);
+				robbie.transform.Translate (-5 * Time.deltaTime, 0, 0);
+				yvette.transform.Translate (-5 * Time.deltaTime, 0, 0);
 				yield return new WaitForEndOfFrame();
 			}
 
