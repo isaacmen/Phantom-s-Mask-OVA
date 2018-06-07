b66af6725aaefcfc047c60e93ccf2ab89e271326
Isaac <isacmen@gmail.com>
2018-06-06 17:09:19 -0700

    final push for now maybe

---
+++ Phantom's Mask OVA/Assets/Scripts/dontdestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dontdestroy : MonoBehaviour {
+
+	private bool created = false;
+
+	// Use this for initialization
+	void Awake () {
+		if (!created) {
+			DontDestroyOnLoad (this.gameObject);
+			created = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
